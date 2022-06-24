6b206a5c2728610a9bbfbe14c742c365db179aed
Vitória Souza <viihsouza161@gmail.com>
2022-06-24 10:37:19 -0300

    added video using vimejs

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,11 +1,19 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import { Button } from "./Button";
 import { Wallpaper } from "./Wallpaper";
+
+import "@vime/core/themes/default.css"
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="SO4-izct7Mc" />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
