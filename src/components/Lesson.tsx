1d60fd64add53e516b0df636a789b30dbc867b61
Vitória Souza <viihsouza161@gmail.com>
2022-06-25 09:47:25 -0300

    added lock when last lesson is not available

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -22,12 +22,14 @@ export function Lesson(props: LessonProps) {
   })
 
   return (
-    <Link to={`/event/lesson/${props.slug}`} className="group">
+    <Link to={`${isLessonAvailable ? `/event/lesson/${props.slug}` : ""}`} className="group">
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
-      <div className={classNames("rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500", {
-        "bg-green-500": isActiveLesson
+      <div className={classNames("rounded border border-gray-500 p-4 mt-2 ", {
+        "bg-green-500": isActiveLesson,
+        "group-hover:border-green-500": isLessonAvailable,
+        "cursor-not-allowed": !isLessonAvailable
       })}>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
