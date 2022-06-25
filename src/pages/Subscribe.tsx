9e1d02033b94d8ab25d3a51ec59c098e76b5651d
Vitória Souza <viihsouza161@gmail.com>
2022-06-25 11:53:04 -0300

    fix code mockup image import

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import codeMockup from "../assets/code-mockup.png"
 
 export function Subscribe() {
   const [name, setName] = useState("")
@@ -64,7 +65,7 @@ export function Subscribe() {
           </form>
         </div>
       </div>
-      <img src="/src/assets/code-mockup.png" alt="" className="mt-10" />
+      <img src={codeMockup} alt="" className="mt-10" />
     </div>
   )
 }
