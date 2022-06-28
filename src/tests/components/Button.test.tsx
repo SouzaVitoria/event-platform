7bb7df20f0b8987d5e40079c93e195d7a06e326c
Vitória Souza <viihsouza161@gmail.com>
2022-06-28 11:47:26 -0300

    tests: Button component

--- src/tests/components/Button.test.tsx
+++ src/tests/components/Button.test.tsx
@@ -2,9 +2,29 @@ import { render, screen } from "@testing-library/react"
 import { Button } from "../../components/Button"
 
 describe("<Button />", () => {
-  it("renders correctly", () => {
+  it("should render correctly", () => {
+    render(<Button text="button-text" variant="secondary" />)
+
+    const button = screen.getByRole('link', { name: /button\-text/i })
+
+    expect(button).toBeInTheDocument()
+  })
+
+  it("should to have the props of the primary variant", () => {
     render(<Button text="button-text" variant="primary" />)
 
-    expect(screen.getByText("button-text")).toBeInTheDocument()
+    const getByText = screen.getByText("button-text")
+    const variantPrimaryClass = "bg-green-500"
+
+    expect(getByText).toHaveClass(variantPrimaryClass)
+  })
+
+  it("should to have the props of the secondary variant", () => {
+    render(<Button text="button-text" variant="secondary" />)
+
+    const getByText = screen.getByText("button-text")
+    const variantSecondaryClass = "border-blue-500"
+
+    expect(getByText).toHaveClass(variantSecondaryClass)
   })
 })
